3fe9976a8b135957b3e21a88a2a1252c619730db
Isaac Reji <isaac.reji@kalvium.community>
2024-12-04 15:48:40 +0530

    changes made in the api file

--- server.js
+++ server.js
@@ -50,6 +50,96 @@ List of Status Codes to Handle:
 200, 201, 204, 400, 401, 403, 404, 405, 429, 500, 502, 503, 504
 */
 
+app.get("/status-info",(req,res)=>{
+  let code=req.query.code
+  if (code==200){
+    let obj={
+      "status": code,
+      "message": "OK: The request has succeeded. The meaning of this status depends on the HTTP method used."
+    }
+    return res.send(obj)
+  }
+  else if (code==201){
+    let obj={
+      "status":code,
+      "message":"request has been successfully fulfilled and has resulted in the creation of a new resource"
+    }
+    return res.send(obj)
+  }
+  else if (code==204){
+    let obj={
+      "status":code,
+      "message":"server has successfully processed the request, but there is no content to send in the response body"
+    }
+    return res.send(obj)
+  }
+  else if (code==400){
+    let obj={
+      "status": code,
+      "message": "Bad Request: The server cannot process the request due to client-side errors (e.g., malformed syntax)."
+    }
+    return res.send(obj)
+  }
+  else if (code==401){
+     let obj={ "status": code,
+      "message":"request has not been applied because it lacks valid authentication credentials or the provided credentials are invalid."
+     }
+     return res.send(obj)
+  }
+  else if (code==403){
+    let obj={ "status": code,
+      "message":" the server understands the request but refuses to authorize it"
+     }
+     return res.send(obj)
+  }
+  else if (code==405){
+    let obj={ "status": code,
+      "message":"the server recognizes the request's HTTP method but does not support it for the targeted resource"
+     }
+     return res.send(obj)
+  }
+  else if (code==429){
+    let obj={ "status": code,
+      "message":"the client has sent too many requests in a given amount of time"
+     }
+     return res.send(obj)
+  }
+  else if (code==502){
+    let obj={ "status": code,
+      "message":"the server, while acting as a gateway or proxy, received an invalid response from an upstream server"
+     }
+     return res.send(obj)
+  }
+  else if (code==503){
+    let obj={ "status": code,
+      "message":"the server is currently unable to handle the request"
+     }
+     return res.send(obj)
+  }
+  else if (code==504){
+    let obj={ "status": code,
+      "message":"he server, while acting as a gateway or proxy, did not receive a timely response from the upstream server needed to complete the request"
+     }
+     return res.send(obj)
+  }
+  else if (code ==404){
+    let obj={
+      "status": code,
+      "message": "Not Found: The server has not found anything matching the request URI. This is often caused by a missing page or resource."
+    }
+    return res.send(obj)
+  }
+  else if (code==500){
+    obj={
+      "status": code,
+      "message": "Internal Server Error: The server encountered an unexpected condition that prevented it from fulfilling the request."
+    }
+    return res.send(obj)
+  }
+  else{
+    return res.send("404")
+  }
+})
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Status Code API is running on http://localhost:${PORT}`);
